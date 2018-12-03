215b7e67d4620257469689bbed7b1ddc85e0bb65
jisujeong <jjisu.jeong@samsung.com>
2018-12-03 16:18:39 +0900

    Update sample.py
    it fixes issue #58

--- function/python/brightics/function/transform/sample.py
+++ function/python/brightics/function/transform/sample.py
@@ -9,9 +9,9 @@ def random_sampling(table, group_by=None, **params):
     else:
         return _random_sampling(table, **params)
 
-def _random_sampling(table, axis='0', num_or_frac='num', num=1, frac=0.5, replace=False, seed=None):
+def _random_sampling(table, num_or_frac='num', num=1, frac=0.5, replace=False, seed=None):
     if num_or_frac == 'num':
-        out_table = table.sample(n=num, replace=replace, random_state=seed, axis=int(axis))
+        out_table = table.sample(n=num, replace=replace, random_state=seed)
     else:  # 'frac'
-        out_table = table.sample(frac=frac, replace=replace, random_state=seed, axis=int(axis))
+        out_table = table.sample(frac=frac, replace=replace, random_state=seed)
     return {'table' : out_table}
