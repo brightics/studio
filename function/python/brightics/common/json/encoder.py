5a6c0497c001be255489b1206f1cb3a2f8e9f4ff
Hyunjoo Kim <hjoo88.kim@samsung.com>
2018-12-05 16:55:36 +0900

    Replace numpy nan to None before encode as json str

--- function/python/brightics/common/json/encoder.py
+++ function/python/brightics/common/json/encoder.py
@@ -3,24 +3,31 @@ import pickle
 import numpy
 from brightics.common.report import ReportBuilder
 
-# DefaultEncoder is used for building viewable json string for in browser
+
+def _to_default_list(np_arr):
+    return numpy.where(numpy.isnan(np_arr), None, np_arr).tolist()
+
+
 class DefaultEncoder(json.JSONEncoder):
+    """
+    DefaultEncoder is used for building viewable json string for in browser
+    """
     def default(self, obj):
+        # TODO add more support types
         if isinstance(obj, set):
             return list(obj)
         elif isinstance(obj, numpy.ndarray):
-            return obj.tolist()
-        # TODO add more support types
+            return _to_default_list(obj)
         else:
-        #elif hasattr(obj, '__str__'):
             rb = ReportBuilder()
             rb.addRawTextMD(str(obj))
-            return {'type':'python object', 'report':rb.get()}
+            return {'type': 'python object', 'report': rb.get()}
 
-     #   return 'python object'
 
-# PickleEncoder is used for building json string saved in redis
 class PickleEncoder(DefaultEncoder):
+    """
+    PickleEncoder is used for building json string saved in redis
+    """
     def encode(self, obj):
         def hint_tuples(item):
             if isinstance(item, tuple):
@@ -38,24 +45,23 @@ class PickleEncoder(DefaultEncoder):
         return super(DefaultEncoder, self).encode(hint_tuples(obj))
 
     def default(self, o):
+        # TODO add more support types
         if isinstance(o, set):
             return {'__set__': list(o)}
         elif isinstance(o, numpy.ndarray):
-            return {'__numpy__': o.tolist()}
-        # TODO add more support types
-        #return {'__pickled__': list(pickle.dumps(o))}
+            return {'__numpy__': _to_default_list(o)}
         elif hasattr(o, '_repr_html_'):
             rb = ReportBuilder()
             rb.addHTML(o._repr_html_())
-            return {'report':rb.get(), '__pickled__': list(pickle.dumps(o))}
+            return {'report': rb.get(), '__pickled__': list(pickle.dumps(o))}
         elif hasattr(o, 'savefig'):
             rb = ReportBuilder()
             rb.addPlt(o)
-            return {'report':rb.get(), '__pickled__': list(pickle.dumps(o))}
+            return {'report': rb.get(), '__pickled__': list(pickle.dumps(o))}
         else:
             rb = ReportBuilder()
             rb.addRawTextMD(str(o))
-            return {'report':rb.get(), '__pickled__': list(pickle.dumps(o))}
+            return {'report': rb.get(), '__pickled__': list(pickle.dumps(o))}
 
 
 def encode(obj, for_redis):
