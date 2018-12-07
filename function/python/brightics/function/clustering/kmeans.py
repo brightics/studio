cd111d3d23d34f2f0d603ce0f8a53790741ff24f
jinhwanhan <jinhwan.han@samsung.com>
2018-12-07 15:18:22 +0900

    validation 추가

--- function/python/brightics/function/clustering/kmeans.py
+++ function/python/brightics/function/clustering/kmeans.py
@@ -10,7 +10,7 @@ from brightics.function.utils import _model_dict
 from brightics.common.groupby import _function_by_group
 from brightics.common.utils import check_required_parameters
 from brightics.function.validation import validate, greater_than_or_equal_to,\
-    greater_than, all_elements_greater_than
+    greater_than, all_elements_greater_than, raise_runtime_error
 
 
 def _kmeans_centers_plot(input_cols, cluster_centers):
@@ -153,7 +153,8 @@ def _kmeans_predict(table, model, prediction_col='prediction'):
         out_table = table.copy()
         out_table[prediction_col] = predict
     else:
-        raise Exception("Unsupported model")
+        raise_runtime_error("Unsupported model")
+        #raise Exception("Unsupported model")
     
     return {'out_table':out_table}
 
--- function/python/brightics/function/validation.py
+++ function/python/brightics/function/validation.py
@@ -9,69 +9,72 @@ def validate(*bfe):
     if len(elist) > 0:
         raise BrighticsFunctionException.from_errors(elist)
 
-def error(true_condition, error_code, error_message_params):
+def get_error(true_condition, error_code, error_message_params):
     if not true_condition:
         return (error_code, error_message_params)
     else:
         None
-        
-def default_error(true_condition, error_message):
-    error(true_condition, '0100', [error_message])
+
+
+def error(error_code, error_message_params, true_condition=False):
+    return get_error(true_condition, error_code, error_message_params)
+
+def runtime_error(error_message, true_condition=False):
+    return get_error(true_condition, '0100', [error_message])
     
 def greater_than(var, criteria, var_name):
-    return error(var > criteria, '0008', [var_name, criteria])
+    return get_error(var > criteria, '0008', [var_name, criteria])
 
 def greater_than_or_equal_to(var, criteria, var_name):
-    return error(var >= criteria, '0010', [var_name, criteria])
+    return get_error(var >= criteria, '0010', [var_name, criteria])
 
 def less_than(var, criteria, var_name):
-    return error(var < criteria, '0012', [var_name, criteria])
+    return get_error(var < criteria, '0012', [var_name, criteria])
 
 def less_than_or_equal_to(var, criteria, var_name):
-    return error(var <= criteria, '0014', [var_name, criteria])
+    return get_error(var <= criteria, '0014', [var_name, criteria])
 
 def from_to(var, from_v, to_v, var_name):
-    return error(from_v <= var <= to_v, '0004', [var_name, from_v, to_v])
+    return get_error(from_v <= var <= to_v, '0004', [var_name, from_v, to_v])
 
 def from_under(var, from_v, under_v, var_name):
-    return error(from_v <= var < under_v, '0006', [var_name, from_v, under_v])
+    return get_error(from_v <= var < under_v, '0006', [var_name, from_v, under_v])
 
 def over_to(var, over_v, to_v, var_name):
-    return error(over_v < var <= to_v, '0006', [var_name, over_v, to_v])
+    return get_error(over_v < var <= to_v, '0006', [var_name, over_v, to_v])
 
 def over_under(var, over_v, under_v, var_name):
-    return error(over_v < var < under_v, '0006', [var_name, over_v, under_v])
+    return get_error(over_v < var < under_v, '0006', [var_name, over_v, under_v])
 
 def all_elements_greater_than(var, criteria, var_name):
-    return error(all([x > criteria for x in var]), '0009', [var_name, criteria])
+    return get_error(all([x > criteria for x in var]), '0009', [var_name, criteria])
 
 def all_elements_greater_than_or_equal_to(var, criteria, var_name):
-    return error(all([x >= criteria for x in var]), '0011', [var_name, criteria])
+    return get_error(all([x >= criteria for x in var]), '0011', [var_name, criteria])
 
 def all_elements_less_than(var, criteria, var_name):
-    return error(all([x < criteria for x in var]), '0013', [var_name, criteria])
+    return get_error(all([x < criteria for x in var]), '0013', [var_name, criteria])
 
 def all_elements_less_than_or_equal_to(var, criteria, var_name):
-    return error(all([x <= criteria for x in var]), '0015', [var_name, criteria])
+    return get_error(all([x <= criteria for x in var]), '0015', [var_name, criteria])
 
 def all_elements_from_to(var, from_v, to_v, var_name):
-    return error(all([from_v <= x <= to_v for x in var]), '0005', [var_name, from_v, to_v])
+    return get_error(all([from_v <= x <= to_v for x in var]), '0005', [var_name, from_v, to_v])
 
 def all_elements_from_under(var, from_v, under_v, var_name):
-    return error(all([from_v <= var < under_v for x in var]), '0007', [var_name, from_v, under_v])
+    return get_error(all([from_v <= var < under_v for x in var]), '0007', [var_name, from_v, under_v])
 
 def all_elements_over_to(var, over_v, to_v, var_name):
-    return error(all([over_v < var <= to_v for x in var]), '0017', [var_name, over_v, to_v])
+    return get_error(all([over_v < var <= to_v for x in var]), '0017', [var_name, over_v, to_v])
 
 def all_elements_over_under(var, over_v, under_v, var_name):
-    return error(all([over_v < var < under_v for x in var]), '0019', [var_name, over_v, under_v])
+    return get_error(all([over_v < var < under_v for x in var]), '0019', [var_name, over_v, under_v])
 
 
-
-def raise_error(true_condition, error_code, error_message_params):
+def raise_error(error_code, error_message_params, true_condition=False):
     if not true_condition:
         raise BrighticsFunctionException(error_code, error_message_params)
 
-def raise_default_error(true_condition, error_message):
+def raise_runtime_error(error_message, true_condition=False):
     if not true_condition:
         raise BrighticsFunctionException('0100', [error_message])
