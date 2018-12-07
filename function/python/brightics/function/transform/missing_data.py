c459d7b6720d4952533e304f9fea9e30fdd679ca
jinhwanhan <jinhwan.han@samsung.com>
2018-12-07 10:32:36 +0900

    validation 추가

--- function/python/brightics/function/transform/missing_data.py
+++ function/python/brightics/function/transform/missing_data.py
@@ -3,13 +3,10 @@ from brightics.function.validation import validate, greater_than_or_equal_to
 
 def delete_missing_data(table, input_cols, how='any', thresh=None):
     
-    validate(greater_than_or_equal_to(thresh, 1, 'thresh'))
-    #validate_number_greater_than_or_equal_to(thresh, 1, 'thresh')
-    #if not thresh >= 1: raise BrighticsFunctionException('0010', ['thresh', 1])
-    
     _table = table.copy()
     
     if thresh is not None:
+        validate(greater_than_or_equal_to(thresh, 1, 'thresh'))
         thresh = len(input_cols) - thresh + 1
     
     _out_table = _table.dropna(subset=input_cols, how=how, axis='index', thresh=thresh)
--- function/python/brightics/function/validation.py
+++ function/python/brightics/function/validation.py
@@ -43,25 +43,10 @@ def over_under(var, over_v, under_v, var_name):
     return error(over_v < var < under_v, '0006', [var_name, over_v, under_v])
 
 
-def raise_exception(true_condition, error_code, error_message_params):
+def raise_error(true_condition, error_code, error_message_params):
     if not true_condition:
         raise BrighticsFunctionException(error_code, error_message_params)
 
-def raise_default_exception(true_condition, error_message):
+def raise_default_error(true_condition, error_message):
     if not true_condition:
         raise BrighticsFunctionException('0100', [error_message])
-
-def raise_greater_than(var, criteria, var_name):
-    raise_exception(var > criteria, '0008', [var_name, criteria])
-
-def raise_greater_than_or_equal_to(var, criteria, var_name):
-    raise_exception(var >= criteria, '0010', [var_name, criteria])
-    
-def raise_less_than(var, criteria, var_name):
-    raise_exception(var < criteria, '0012', [var_name, criteria])
-    
-def raise_less_than_or_equal_to(var, criteria, var_name):
-    raise_exception(var <= criteria, '0014', [var_name, criteria])
-    
-def raise_from_to(var, from_v, to_v, var_name):
-    raise_exception(var >= from_v and var <= to_v, '0004', [var_name, from_v, to_v])
