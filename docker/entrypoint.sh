31a26d75141d4ef05338eb5633088327d589a6d3
kwangsun <kwangsun.noh@samsung.com>
2018-12-10 02:18:55 +0000

    remove sleep statement

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -17,5 +17,11 @@ rm -rf /brightics-studio/brightics-server/data
 ln -s /brightics-studio/userdata/data /brightics-studio/brightics-server/data
 
 sh start-brightics.sh
-sleep 2
+
+mkdir -p /brightics-studio/brightics-server/logs \
+         /brightics-studio/visual-analytics/logs
+touch /brightics-studio/brightics-server/logs/brightics-server.log \
+      /brightics-studio/visual-analytics/logs/visual-analytics.client.log \
+      /brightics-studio/visual-analytics/logs/visual-analytics.error.log \
+      /brightics-studio/visual-analytics/logs/visual-analytics.log
 exec tail -f /brightics-studio/brightics-server/logs/* /brightics-studio/visual-analytics/logs/*
