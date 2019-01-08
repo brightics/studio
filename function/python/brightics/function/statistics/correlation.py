d99910156f72efade2f06fa510969af60f09dbbf
Sungjun Kim <sungjun5.kim@samsung.net>
2019-01-08 15:42:23 +0900

    delete corr, pvalues and add corr_table containing corr, pvalues to json object

--- function/python/brightics/function/statistics/correlation.py
+++ function/python/brightics/function/statistics/correlation.py
@@ -1,10 +1,11 @@
 import matplotlib.pyplot as plt
 import seaborn as sns
-from brightics.common.report import ReportBuilder
+from brightics.common.report import ReportBuilder, strip_margin, plt2MD,\
+    pandasDF2MD
 from scipy import stats
-import numpy as np
 from brightics.common.groupby import _function_by_group
 from brightics.common.utils import check_required_parameters
+import pandas as pd
 
 
 def correlation(table, group_by=None, **params):
@@ -14,14 +15,14 @@ def correlation(table, group_by=None, **params):
     else:
         return _correlation(table, **params)
 
+
 def _correlation(table, vars, method='pearson', height=2.5, corr_prec=2):
     size = len(vars)
     
     s_default = plt.rcParams['lines.markersize'] ** 2.
-    scatter_kws={"s": s_default*height/6.4}
+    scatter_kws = {"s": s_default * height / 6.4}
     
-    corr_arr = np.ones((size, size))  # TODO variable name dict
-    p_arr = np.zeros((size, size))
+    result_arr = []
     
     for i in range(size): 
         for j in range(i):
@@ -31,15 +32,11 @@ def _correlation(table, vars, method='pearson', height=2.5, corr_prec=2):
                 r, p = stats.spearmanr(table[vars[i]], table[vars[j]])
             elif method == 'kendal':
                 r, p = stats.kendalltau(table[vars[i]], table[vars[j]])
-                
-            corr_arr[i][j] = r
-            p_arr[i][j] = p
+            
+            result_arr.append([vars[i], vars[j], r, p])    
+            
+    df_result = pd.DataFrame(result_arr, columns=['x', 'y', 'corr', 'p_value'])
     
-    for i in range(size):
-        for j in range(i, size):
-            corr_arr[i][j] = corr_arr[j][i]
-            p_arr[i][j] = p_arr[j][i]
-        
     def corr(x, y, **kwargs):
         if kwargs['method'] == 'pearson':
             r, p = stats.pearsonr(x, y)
@@ -57,8 +54,7 @@ def _correlation(table, vars, method='pearson', height=2.5, corr_prec=2):
             p_stars = '***'
             
         corr_text = '{:.{prec}f}'.format(r, prec=corr_prec)
-        print(type(corr_prec))
-        font_size = abs(r) * 15 * 2/corr_prec + 5
+        font_size = abs(r) * 15 * 2 / corr_prec + 5
         ax = plt.gca()
         ax.annotate(corr_text, [.5, .5, ], xycoords="axes fraction",
                     ha='center', va='center', fontsize=font_size * height)
@@ -72,16 +68,24 @@ def _correlation(table, vars, method='pearson', height=2.5, corr_prec=2):
         g.map_lower(sns.regplot, lowess=True, scatter_kws=scatter_kws) 
     g.map_upper(corr, method=method)
     
-    rb = ReportBuilder()
-    rb.addPlt(plt)
+    fig_corr = plt2MD(plt)
     plt.clf()
+    
+    rb = ReportBuilder()
+    rb.addMD(strip_margin(
+        """ ## Correlation Results
+        | ### Correlation Matrix
+        | {fig_corr}
+        |
+        | ### Correlation Table
+        | {table}
+        """.format(fig_corr=fig_corr, table=pandasDF2MD(df_result))))
     
     params = {'vars':vars, 'method':method, 'height':height}
     
     res = dict()
     res['params'] = params
-    res['corr'] = corr_arr
-    res['pvalue'] = p_arr
+    res['corr_table'] = df_result
     res['report'] = rb.get()
     
     return {'result': res}
