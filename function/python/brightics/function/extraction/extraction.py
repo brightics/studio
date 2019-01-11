0cf9c6d7d90a05b1d0634c1514838ca76ffa52bc
Sungjun Kim <sungjun5.kim@samsung.net>
2019-01-11 15:55:06 +0900

    extraction refactoring. cnt now uses collections.Counter. report -> repr

--- function/python/brightics/function/extraction/extraction.py
+++ function/python/brightics/function/extraction/extraction.py
@@ -1,9 +1,12 @@
-from brightics.common.repr import BrtcReprBuilder, strip_margin, plt2MD, dict2MD, \
-    pandasDF2MD, keyValues2MD
+from brightics.common.repr import BrtcReprBuilder
+from brightics.common.repr import strip_margin
+from brightics.common.repr import dict2MD
+from brightics.common.repr import pandasDF2MD
 from brightics.function.utils import _model_dict
 from brightics.common.groupby import _function_by_group
 from brightics.common.utils import check_required_parameters
 
+from collections import Counter
 import pandas as pd
 import numpy as np
 
@@ -20,7 +23,7 @@ def _add_row_number(table, new_col='add_row_number'):
     n = len(table)
     out_table = table.copy()
     out_table[new_col] = range(n)
-    columns = table.columns.insert(0,new_col)
+    columns = table.columns.insert(0, new_col)
     out_table = out_table.reindex(columns=columns)
     return {'out_table': out_table}
 
@@ -37,43 +40,45 @@ def _discretize_quantile(table, input_col, num_of_buckets=2, out_col_name='bucke
     out_table = table.copy()
     out_table[out_col_name], buckets = pd.qcut(table[input_col], num_of_buckets, labels=False, retbins=True, precision=10, duplicates='drop')    
                 
+    params = { 
+        'input_col': input_col,
+        'num_of_buckets': num_of_buckets,
+        'out_col_name': out_col_name
+    }
+
+    cnt = Counter(out_table[out_col_name].values)
+    
     # index_list, bucket_list
     index_list = []
-    bucket_list = []     
-    for i in range(len(buckets)):
-        if i == 1:
-            left = '['
-        else:
-            left = '('
-    
-        if i > 0:
-            index_list.append(i - 1)
-            bucket_list.append("{left}{lower}, {upper}]".format(left=left, lower=buckets[i - 1], upper=buckets[i]))  # 'buckets' is tuple type data.  
-              
-    # cnt_array
-    cnt = np.zeros(len(index_list), int)
-    for i in range(len(table)):
-            cnt[out_table[out_col_name][i]] += 1
+    bucket_list = []
+    cnt_list = []     
+    for i in range(len(buckets)-1):
+        left = '[' if i == 0 else '('
+        index_list.append(i)
+        cnt_list.append(cnt[i])
+        bucket_list.append("{left}{lower}, {upper}]".format(left=left, lower=buckets[i], upper=buckets[i+1]))  # 'buckets' is tuple type data.  
    
     # Build model
     result = pd.DataFrame.from_items([ 
         ['bucket number', index_list],
         ['buckets', bucket_list],
-        ['count', cnt]
+        ['count', cnt_list]
     ])
     
     # Build model
     rb = BrtcReprBuilder()
     rb.addMD(strip_margin("""
-    ## Quantile-based Discretization Result
-    ### Data = {input_col}
-    |
+    | ## Quantile-based Discretization Result
+    | ### Result
     | {result}
-    """.format(input_col=input_col, n=num_of_buckets, result=pandasDF2MD(result))))
+    |
+    | ### Parameters
+    | {params} 
+    """.format(result=pandasDF2MD(result), params=dict2MD(params))))
     
-    model = _model_dict()
+    model = _model_dict('discretize_quantile')
     model['result'] = result
-    model['report'] = rb.get()
+    model['_repr_brtc_'] = rb.get()
     
     return {'out_table': out_table, 'model': model}
 
