9b6071613057c60b8f9be80b4b557c5dd69bb446
iisejin <44356894+iisejin@users.noreply.github.com>
2019-01-14 21:49:04 +0900

    Patch visual-analytics #3 (#208)
    * Patch visual-analytics

---
+++ va-server/visual-analytics/lib/transform-model.js
@@ -0,0 +1,54 @@
+/* -----------------------------------------------------
+ *  transform-model.js
+ *  Created by hyunseok.oh@samsung.com on 2018-07-23.
+ * ---------------------------------------------------- */
+
+var ACTION_KEY = '__brtc__model__sync__';
+
+function transformModel(model, operationTree) {
+    var keys = Object.keys(operationTree);
+    var keyLen = keys.length;
+    var action;
+    var key;
+    var child;
+
+    if (Array.isArray(model)) {
+        for (let i = 0; i < keyLen; i++) {
+            key = keys[i];
+            child = operationTree[key];
+            if (child.hasOwnProperty(ACTION_KEY)) {
+                action = child.action;
+                if (action === 'update') {
+                    model[key] = child.val;
+                } else if (action === 'remove') {
+                    model.pop();
+                } else if (action === 'add') {
+                    model.push(child.val);
+                    if (model.length - 1 != key) {
+                        throw new Error('ERRRRR');
+                    }
+                }
+            } else {
+                model[key] = transformModel(model[key], child);
+            }
+        }
+    } else {
+        for (let i = 0; i < keyLen; i++) {
+            key = keys[i];
+            child = operationTree[key];
+            if (child.hasOwnProperty(ACTION_KEY)) {
+                action = child.action;
+                if (action === 'update' || action === 'add') {
+                    model[key] = child.val;
+                } else if (action === 'remove') {
+                    delete model[key];
+                }
+            } else {
+                model[key] = transformModel(model[key], child);
+            }
+        }
+    }
+    return model;
+}
+
+module.exports = transformModel;
