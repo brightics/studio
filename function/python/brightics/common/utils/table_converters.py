e933de46fdc705a45537dc51f10c425cf28dd861
Sungjun Kim <sungjun5.kim@samsung.net>
2019-01-14 20:07:26 +0900

    add drop_index

--- function/python/brightics/common/utils/table_converters.py
+++ function/python/brightics/common/utils/table_converters.py
@@ -33,13 +33,19 @@ def _to_float(df):
     return df
 
 
-def simple_tables2df_list(simple_tables):
+def simple_tables2df_list(simple_tables, drop_index=False):
     table_list = []
     for simple_table in simple_tables:
         header = _replace_column_names(_header(simple_table))
         if header is not None:
-            data = [row.data for row in simple_table[1:]]
-            table_list.append(_to_float(pd.DataFrame(data, columns=header)))
+            simple_table = simple_table[1:] # header
+            if drop_index:
+                index = [row.data[0] for row in simple_table]
+                data = [row.data[1:] for row in simple_table]
+                table_list.append(_to_float(pd.DataFrame(data, columns=header[1:], index=index)))
+            else:
+                data = [row.data for row in simple_table]
+                table_list.append(_to_float(pd.DataFrame(data, columns=header)))
         else:
             data = [row.data for row in simple_table]
             table_list.append(_to_float(pd.DataFrame(data)))
