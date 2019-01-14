3cd8d0027e9f3a583dbfbcfdad01ea0068339990
sungjin1.kim <sungjin1.kim@samsung.com>
2019-01-14 19:03:58 +0900

    Patch visual-analytics

--- va-server/visual-analytics/routes/api/va/v3/functions.js
+++ va-server/visual-analytics/routes/api/va/v3/functions.js
@@ -1,8 +1,8 @@
 const locations = {
     js: [   './public/js/va/functions/',
         './public/js/va/addonfunctions/'],
-    json: [ './public/js/va/functions/json/',
-        './public/js/va/addonfunctions/json/'],
+    // json: [ './public/js/va/functions/json/',
+    //     './public/js/va/addonfunctions/json/'],
 };
 const request = require('request');
 const getPaletteByModelType = require('../../../../lib/merge-palette');
@@ -11,7 +11,6 @@ const getFileListPromise = (dir = '', type = '') => new Promise((resolve, reject
     if (!__REQ_fs.existsSync(dir)) {
         __REQ_fs.mkdirSync(dir);
     }
-
     __REQ_fs.readdir(dir, (err, files) => {
         if (err) {
             if (err.code !== 'ENOENT') console.error(err)
@@ -26,18 +25,7 @@ const getFileListbyType = async (dirs = [], type = '') => {
     let list = dirs.map(dir => getFileListPromise(dir, type));
     let files = await Promise.all(list);
     return files
-}
-
-const getFilePromise = (filename = '', type = '') => new Promise( (resolve, reject) => {
-    __REQ_fs.readFile(filename, (err, file) => {
-        if (err) {
-            if (err.code !== 'ENOENT') console.error(err)
-            reject()
-        } else {
-            resolve(file);
-        }
-    })
-})
+};
 
 const DBLoadPromise = (req, res, palette) => new Promise((resolve, reject) => {
     const options = __BRTC_API_SERVER.createRequestOptions('GET', '/api/vastudio/v3/udfs');
@@ -81,44 +69,36 @@ const JSLoader = (palette, list) => {
             // console.log(action)
         });
 };
-const JSONLoader = async (palette, list) => {
-    const target = 'json';
-    const fullFilePath = list.map((item, index) =>
-        item.map(filename => locations[target][index] + filename))
-        .reduce((statement, element) => [...statement, ...element], []);
-    const FileLoadPromise = fullFilePath
-        .map(file => getFilePromise(file))
-    const loading = await Promise.all(FileLoadPromise)
-    const fileContents = loading.filter(item => {
-        try {
-            JSON.parse(item)
-            return true
-        } catch (e) {
-            return false
+const getFileContentsViaCorePromise = (req, res) => new Promise((resolve, reject) => {
+    const options = __BRTC_CORE_SERVER.createRequestOptions('GET', '/api/core/v3/function/meta');
+    __BRTC_CORE_SERVER.setBearerToken(options, req.accessToken);
+    request(options, function (error, response) {
+        if (error) {
+            reject(response);
+        } else {
+            if (response.statusCode == 200) {
+                const list = JSON.parse(response.body);
+                resolve(Object.values(list) || []);
+            } else {
+                reject(response.statusCode);
+            }
         }
     })
-        .map(JSON.parse)
-    fileContents.forEach(file => {
-        const category = file.specJson.category;
-        const name = file.specJson.func;
-        const item = {func: name, visible: true};
-        const list = palette.filter(p => p.key === category)
-        // const action =
-            list.reduce((s, i) => s || !i.functions.some(obj => obj.func === name), false) ?
-                list.map(obj => obj.functions.push(item)) && `success ${name}` :
-                `fail ${name}`;
-    })
-    return fileContents;
+});
+
+const JSONLoader = async (req, res) => {
+    return await getFileContentsViaCorePromise(req, res);
 }
 
+// returning value : Promise
 const makeCustomPalette = async (req, res, palette) => {
     const list = Object
         .entries(locations)
         .map(entry => getFileListbyType(entry[1], entry[0]))
-    const [js_chunks, json_chunks] = await Promise.all(list)
+    const [js_chunks] = await Promise.all(list)
     // done[0]은 js묶음, done[1]은 json묶음
     JSLoader(palette, js_chunks);
-    const [fileContents, dbContents] = await Promise.all([JSONLoader(palette, json_chunks), DBLoadPromise(req, res, palette)])
+    const [fileContents, dbContents] = await Promise.all([JSONLoader(req, res), DBLoadPromise(req, res, palette)])
     return {
         palette,
         fileContents: fileContents || [],
@@ -126,7 +106,7 @@ const makeCustomPalette = async (req, res, palette) => {
     };
 }
 
-
+// returning value : Promise
 exports.makePalette = async (req, res, palette = [], type = 'data') => {
     if (type === 'data') {
         const result = await makeCustomPalette(req, res, palette)
@@ -135,21 +115,26 @@ exports.makePalette = async (req, res, palette = [], type = 'data') => {
     return undefined;
 }
 
+// returning value : Promise
 exports.getPaletteModelType = (req, res, modelType = '') =>
     new Promise((resolve, reject) => {
         const palette = getPaletteByModelType(modelType)
         resolve({palette})
     });
 
+// returning value : Promise
 exports.getPalette = function (req, res) {
     const palette = getPaletteByModelType('data')
     return makeCustomPalette(req, res, palette)
 };
 
+// router function for express
+// TODO: express에서는 비동기 router function을 이루기위해 asyncnify가 필요하다.
+//       다른 async지원을 충분히 가능하도록 express에서 지원을 할때 전환 필요 or
+//       자체 asyncnify나 promisify을 구현 할 필요가 있다.
 exports.listFunctions = function (req, res) {
-    const palette = getPaletteByModelType('data')
-    makeCustomPalette(req, res, palette)
+    Promise.resolve(JSONLoader(req, res))
         .then(s => {
-            res.send(s.fileContents.map(e => e.specJson))
+            res.send(s.map(e => e.specJson))
         })
 };
