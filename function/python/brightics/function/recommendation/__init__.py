b6cbc8f2334adbb25e978018415d68d5eb086bec
power4454 <oper.kang@samsung.com>
2019-01-24 20:40:51 +0900

    change typo on collaborative filtering

--- function/python/brightics/function/recommendation/__init__.py
+++ function/python/brightics/function/recommendation/__init__.py
@@ -2,4 +2,5 @@ from .association_rule import association_rule
 from .association_rule import association_rule_visualization
 from .als import als_train
 from .als import als_predict
-from .collaborative_filtering import collaborative_filtering
+from .collaborative_filtering import collaborative_filtering_train
+from .collaborative_filtering import collaborative_filtering_predict
