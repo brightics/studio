9f516e119ba5aad37b9fd62871a6f945652d0528
jinhwanhan <jinhwan.han@samsung.com>
2019-01-25 11:35:53 +0900

    add random data generating utils

--- function/python/brightics/common/datasets.py
+++ function/python/brightics/common/datasets.py
@@ -3,8 +3,9 @@ import numpy as np
 import random
 import os
 from sklearn.datasets import load_iris as sklearn_load_iris
+import string
 
-TESTDATA_RELATIVEPATH = '''/../../../sample_data'''
+TESTDATA_RELATIVEPATH = '''/../sample_data'''
 
 _PATH_PARQUET = os.path.abspath(os.path.dirname(os.path.abspath(__file__)) +
                                 TESTDATA_RELATIVEPATH)
@@ -21,6 +22,80 @@ def save_dataset(table, fname):
                                    engine='auto', compression='snappy')
 
 
+def load_iris():
+    return load_dataset('iris')
+
+
+def load_basic_test_data(data_size=20, seed=1):
+    _columns = [
+        {'name': 'grp', 'type': 'category', 'population': ['G', 'B']},
+        {'name': 'd1', 'type': 'float', 'min_value':-1000.0,
+         'max_value': 1000.0},
+        {'name': 'd2', 'type': 'float', 'min_value':-1000.0,
+         'max_value': 1000.0, 'n_null': 2},
+        {'name': 'str1', 'type': 'string', 'min_length': 1, 'max_length': 3,
+         'population': ['a', 'b']},
+        {'name': 'str2', 'type': 'string', 'min_length': 5, 'max_length': 10,
+         'population': (string.ascii_uppercase + string.ascii_lowercase +
+                        string.punctuation),
+         'n_null': 3},
+        {'name': 'int1', 'type': 'int', 'min_value': 1, 'max_value': 1000},
+        {'name': 'int2', 'type': 'int', 'min_value':-1000, 'max_value': 1000}
+        ]
+
+    return load_random_table(columns=_columns, seed=seed,
+                             data_size=data_size)
+
+
+def load_random_classification(data_size=200, seed=1, n_xcol=4, n_class=3):
+    _columns = []
+    for i in range(1, n_xcol + 1):
+        _columns.append({
+            'name': 'x{}'.format(i),
+            'type': 'float',
+            'min_value': 0.0,
+            'max_value': 1.0
+            })
+
+    _columns.append({
+            'name': 'y',
+            'type': 'int',
+            'min_value': 0,
+            'max_value': n_class - 1
+        })
+
+    return load_random_table(columns=_columns, seed=seed,
+                             data_size=data_size)
+
+
+def load_random_regression(data_size=200, seed=1, n_xcol=4):
+    _columns = []
+    for i in range(1, n_xcol + 1):
+        _columns.append({
+            'name': 'x{}'.format(i),
+            'type': 'float',
+            'min_value': 0.0,
+            'max_value': 1.0
+            })
+
+    _columns.append({
+            'name': 'y',
+            'type': 'float',
+            'min_value': 0.0,
+            'max_value': 1.0
+        })
+
+    return load_random_table(columns=_columns, seed=seed,
+                             data_size=data_size)
+
+
+def add_category_column(table, group_col_name, population, seed=None):
+    table[group_col_name] = _get_category_column(data_size=len(table),
+                                                 population=population,
+                                                 seed=seed)
+    return table
+
+
 def load_random_table(columns, seed=None, data_size=20, add_index=True):
 
     npr = np.random.RandomState(seed)
@@ -31,28 +106,49 @@ def load_random_table(columns, seed=None, data_size=20, add_index=True):
         col_info['index'] = np.arange(1, data_size + 1, step=1)
 
     for c in columns:
+
+        n_null = 0 if 'n_null' not in c else c['n_null']
+        i_nulls = random.sample(range(0, data_size), n_null)
+
         if c['type'] == 'int':
-            col_info[c['name']] = npr.random_integers(
+            new_col_data = npr.randint(
                 low=c['min_value'], high=c['max_value'], size=data_size)
         elif c['type'] == 'float':
-            col_info[c['name']] = (npr.random_sample(size=data_size)
-                                   * (c['max_value'] - c['min_value'])
-                                   + c['min_value'])
+            new_col_data = (npr.random_sample(size=data_size) *
+                            (c['max_value'] - c['min_value']) +
+                            c['min_value'])
+            for i in i_nulls:
+                new_col_data[i] = np.nan
         elif c['type'] == 'category':
-            col_info[c['name']] = random.choices(population=c['population'],
-                                                 k=data_size)
+            new_col_data = _get_category_column(
+                data_size=data_size,
+                population=c['population'],
+                seed=seed)
         elif c['type'] == 'string':
-            col_info[c['name']] = [_get_randstring(c['population'],
-                                                   c['min_length'],
-                                                   c['max_length'])
-                                   for _ in range(0, data_size)]
+            new_col_data = [_get_randstring(c['population'],
+                                            c['min_length'],
+                                            c['max_length'])
+                            for _ in range(0, data_size)]
+            for i in i_nulls:
+                new_col_data[i] = None
+        else:
+            new_col_data = [None for _ in range(0, data_size)]
+
+        col_info[c['name']] = new_col_data
 
     data = pd.DataFrame(data=col_info)
     return data
 
 
-def load_iris():
-    return load_dataset('iris')
+def load_random_float_table(data_size=20, seed=None, n_col=4,
+                            min_value=0.0, max_value=1.0, add_index=True):
+    cols = []
+    for i in range(1, n_col + 1):
+        c = {'name': 'x{}'.format(i), 'type': 'float', 'min_value': min_value,
+             'max_value': max_value}
+        cols.append(c)
+    return load_random_table(columns=cols, seed=seed, data_size=data_size,
+                             add_index=add_index)
 
 
 def _make_dir(path):
@@ -73,10 +169,17 @@ def _get_randstring(population, min_length, max_length):
     return ''.join(random.choices(population,
                                   k=random.randint(min_length, max_length)))
 
-# REPOSITORY_CSV = REPOSITORY_TESTDATA + '''/csv'''
-# _PATH_FORMAT_CSV = REPOSITORY_CSV + '''/{}.csv'''
-# def _load_dataset_csv(fname, _sep=',', _quotechar='"'):
-#     return pd.read_csv(_PATH_FORMAT_CSV.format(fname), sep=_sep, quotechar=_quotechar)
-# def _save_dataset_csv(table, fname, _sep=',', _quotechar='"', _index=False):
-#     _make_dir(REPOSITORY_CSV)
-#     pd.DataFrame(table).to_csv(_PATH_FORMAT_CSV.format(fname), sep=_sep, quotechar=_quotechar, index=_index)
+
+def _get_category_column(data_size, population, seed=None):
+    random.seed(seed)
+    pop_len = len(population)
+    if pop_len > data_size:
+        raise RuntimeError(
+            '''The data size must be larger than '''
+            '''each category population size.''')
+    temp_category_coldata = (population +
+                             random.choices(
+                                 population=population,
+                                 k=(data_size - pop_len)))
+    random.shuffle(temp_category_coldata)
+    return temp_category_coldata
--- function/python/brightics/common/test/test_datasets.py
+++ function/python/brightics/common/test/test_datasets.py
@@ -1,40 +1,100 @@
 import unittest
 import pandas
 from brightics.common.datasets import load_iris, _load_iris_sklearn, \
-    load_random_table
+    load_random_table, load_random_float_table, add_category_column, \
+    load_basic_test_data, load_random_classification, load_random_regression
 import random
 import string
+import logging
 
 
 class DatasetTest(unittest.TestCase):
 
-    def test_templates(self):
+    def setUp(self):
+        unittest.TestCase.setUp(self)
+        self.logger = logging.getLogger(self.__class__.__name__)
+        self.logger.setLevel(logging.INFO)
+        self.logger.addHandler(logging.StreamHandler())
+
+    def test_iris(self):
         iris_parquet = load_iris()
         iris_sklearn = _load_iris_sklearn()
         pandas.testing.assert_frame_equal(iris_parquet, iris_sklearn)
 
-    def dev_randomdata(self):
+    def test_basic_test_data(self):
+        _seed = random.randint(0, 100)
+        _data_size = random.randint(10, 50)
+        data1 = load_basic_test_data(seed=_seed, data_size=_data_size)
+        data2 = load_basic_test_data(seed=_seed, data_size=_data_size)
+        self.logger.info(data1)
+        pandas.testing.assert_frame_equal(data1, data2)
+
+    def test_load_random_classification(self):
+        _seed = random.randint(0, 100)
+        _data_size = random.randint(150, 500)
+        _n_xcol = random.randint(1, 5)
+        _n_class = random.randint(2, 5)
+        data1 = load_random_classification(seed=_seed, data_size=_data_size,
+                                           n_xcol=_n_xcol,
+                                           n_class=_n_class)
+        data2 = load_random_classification(seed=_seed, data_size=_data_size,
+                                           n_xcol=_n_xcol,
+                                           n_class=_n_class)
+        self.logger.info(data1)
+        pandas.testing.assert_frame_equal(data1, data2)
+
+    def test_load_random_regression(self):
+        _seed = random.randint(0, 100)
+        _data_size = random.randint(150, 500)
+        _n_xcol = random.randint(1, 5)
+        data1 = load_random_regression(seed=_seed, data_size=_data_size,
+                                       n_xcol=_n_xcol)
+        data2 = load_random_regression(seed=_seed, data_size=_data_size,
+                                       n_xcol=_n_xcol)
+        self.logger.info(data1)
+        pandas.testing.assert_frame_equal(data1, data2)
+
+    def test_randomfloat(self):
+        _seed = random.randint(0, 100)
+        _n_col = random.randint(1, 5)
+        rf1 = load_random_float_table(seed=_seed, n_col=_n_col)
+        rf1 = add_category_column(rf1, 'group1', ['A', 'B'], _seed)
+        rf1 = add_category_column(rf1, 'group2', [1, 2], _seed)
+
+        rf2 = load_random_float_table(seed=_seed, n_col=_n_col)
+        rf2 = add_category_column(rf2, 'group1', ['A', 'B'], _seed)
+        rf2 = add_category_column(rf2, 'group2', [1, 2], _seed)
+
+        self.logger.info(rf1)
+        pandas.testing.assert_frame_equal(rf1, rf2)
+
+    def test_randomdata(self):
 
         RANDOM_COLUMN = [
-            {'name': 'group1', 'type': 'category', 'population': ['A', 'B']},
-            {'name': 'group2', 'type': 'category', 'population': [1, 2]},
-            {'name': 'int1', 'type': 'int', 'min_value': -1000,
+            {'name': 'float1', 'type': 'float', 'min_value':-1000.0,
+             'max_value': 1000.0, 'n_null': 5},
+            {'name': 'group1', 'type': 'category',
+             'population': ['A', 'B', 'C']},
+            {'name': 'group2', 'type': 'category', 'population': [1, 0]},
+            {'name': 'int1', 'type': 'int', 'min_value':-1000,
              'max_value': 1000},
-            {'name': 'float1', 'type': 'float', 'min_value': -1000.0,
-             'max_value': 1000.0},
             {'name': 'string1', 'type': 'string', 'min_length': 5,
              'max_length': 5, 'population': string.ascii_uppercase},
             {'name': 'string2', 'type': 'string', 'min_length': 1,
-             'max_length': 20, 'population': (string.ascii_uppercase
-                                              + string.ascii_lowercase
-                                              + string.punctuation)}
+             'max_length': 20, 'n_null': 8,
+             'population': (string.ascii_uppercase +
+                            string.ascii_lowercase +
+                            string.punctuation)}
             ]
 
         _seed = random.randint(0, 100)
-        table = load_random_table(columns=RANDOM_COLUMN, seed=_seed)
+        _data_size = 30
+        table = load_random_table(columns=RANDOM_COLUMN, seed=_seed,
+                                  data_size=_data_size)
         pandas.testing.assert_frame_equal(table,
                                           load_random_table(
                                               columns=RANDOM_COLUMN,
-                                              seed=_seed))
+                                              seed=_seed,
+                                              data_size=_data_size))
         print(_seed)
         print(table)
