a6256f771e2eebd2d027455e31f1f91744cd814c
jooyeon chung <jy1106.chung@samsung.com>
2019-01-28 17:21:16 +0900

    Hierarchical Clustering / Post Process are modified.
    - Hierarchical Clustering Post Process function name is changed.
    init.py, hierarchical_clustering.py, and
    hierarchical_clustering_post.json are changed accordingly.

--- function/python/brightics/function/clustering/__init__.py
+++ function/python/brightics/function/clustering/__init__.py
@@ -2,4 +2,4 @@ from .kmeans import kmeans_train_predict
 from .kmeans import kmeans_predict
 from .kmeans import kmeans_silhouette_train_predict
 from .hierarchical_clustering import hierarchical_clustering
-from .hierarchical_clustering import hierarchical_clustering_post
+from .hierarchical_clustering import hierarchical_clustering_post_process
