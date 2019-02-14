935fc45067fc6b3f07279164dd22213af43a113f
power4454 <45587294+power4454@users.noreply.github.com>
2019-02-14 21:41:39 +0900

    Update json_extraction.py

--- function/python/brightics/function/transform/json_extraction.py
+++ function/python/brightics/function/transform/json_extraction.py
@@ -31,7 +31,8 @@ def _flatten(obj, key=None, flattened_dict=None, sep='__'):
                 _flatten(obj[obj_key], _key(key, sep, obj_key), flattened_dict)
     elif isinstance(obj, set) or isinstance(obj, numpy.ndarray) or isinstance(obj, tuple):
         flattened_dict[key] = list(obj)
-    
+    elif _flattenable(obj):
+        flattened_dict[key] = list(obj)
     return flattened_dict
 
 
