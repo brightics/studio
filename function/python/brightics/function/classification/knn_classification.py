31b67b86aaf124fc178a4f8a570f2466c25612de
jen-choi <jen.choi@samsung.com>
2019-02-26 11:26:25 +0900

    Add check_required_parameters
    This is related to #408

--- function/python/brightics/function/classification/knn_classification.py
+++ function/python/brightics/function/classification/knn_classification.py
@@ -1,9 +1,14 @@
 from sklearn.neighbors import KNeighborsClassifier 
 import pandas as pd
+from brightics.common.utils import check_required_parameters
 
 
+def knn_classification(train_table, test_table, **params):
+    check_required_parameters(_knn_classification, params, ['train_table', 'test_table'])
+    return _knn_classification(train_table, test_table, **params)
 
-def knn_classification(train_table, test_table, feature_cols, label_col, k=5, algorithm='auto', leaf_size=30, p=2, pred_col_name='prediction', prob_col_prefix='probability', suffix='index'):
+
+def _knn_classification(train_table, test_table, feature_cols, label_col, k=5, algorithm='auto', leaf_size=30, p=2, pred_col_name='prediction', prob_col_prefix='probability', suffix='index'):
     
     X_train = train_table[feature_cols]
     y_train = train_table[label_col]
