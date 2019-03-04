3247425c0c1abc5ca499b7df382824c7fa26fba1
jen-choi <jen.choi@samsung.com>
2019-03-04 15:20:40 +0900

    Add param_validation_check

--- function/python/brightics/function/regression/knn_regression.py
+++ function/python/brightics/function/regression/knn_regression.py
@@ -1,9 +1,24 @@
 from sklearn.neighbors import KNeighborsRegressor 
 import pandas as pd
-import numpy as np
+from brightics.common.utils import check_required_parameters
+from brightics.common.utils import get_default_from_parameters_if_required
+from brightics.common.validation import validate
+from brightics.common.validation import greater_than_or_equal_to
 
 
-def knn_regression(train_table, test_table, feature_cols, label_col, k=5, algorithm='auto', leaf_size=30, p=2, pred_col_name='prediction'):
+def knn_regression(train_table, test_table, **params):
+    check_required_parameters(_knn_regression, params, ['train_table', 'test_table'])
+
+    params = get_default_from_parameters_if_required(params,_knn_regression)
+    param_validation_check = [greater_than_or_equal_to(params, 1, 'k'),
+                              greater_than_or_equal_to(params, 1, 'leaf_size'),
+                              greater_than_or_equal_to(params, 1, 'p')]
+    validate(*param_validation_check)
+
+    return _knn_regression(train_table, test_table, **params)
+
+
+def _knn_regression(train_table, test_table, feature_cols, label_col, k=5, algorithm='auto', leaf_size=30, p=2, pred_col_name='prediction'):
     
     X_train = train_table[feature_cols]
     y_train = train_table[label_col]
