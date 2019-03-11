12468a95149617287c5a09441f4620e874cbc449
hoon.im <vosej1vos>
2019-03-11 12:44:50 +0900

    change read moment js at one time.

--- common/workflow/src/main/java/com/samsung/sds/brightics/common/workflow/flowrunner/jslib/MomentJsLibrary.java
+++ common/workflow/src/main/java/com/samsung/sds/brightics/common/workflow/flowrunner/jslib/MomentJsLibrary.java
@@ -1,9 +1,10 @@
 package com.samsung.sds.brightics.common.workflow.flowrunner.jslib;
 
+import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.io.Reader;
 import java.util.Set;
+import java.util.StringJoiner;
 import java.util.regex.Pattern;
 
 import org.mozilla.javascript.Context;
@@ -19,26 +20,42 @@ import com.samsung.sds.brightics.common.variable.scope.VariableScope;
 
 public class MomentJsLibrary implements JsLibrary {
 
-    private static final Logger LOGGER = LoggerFactory.getLogger(MomentJsLibrary.class);
+	private static final Logger LOGGER = LoggerFactory.getLogger(MomentJsLibrary.class);
+	private static final String LINE_SEPARATOR = System.getProperty("line.separator");
 
-    @Override
-	public void loadLibrary(VariableScope variableScope) {
-		Context.enter();
+	private String momentJsString;
 
-		Reflections reflections = new Reflections(new ConfigurationBuilder().setScanners(new ResourcesScanner())
-				.setUrls(ClasspathHelper.forPackage("com.samsung.sds.brightics.common.workflow.flowrunner.jslib")));
-		Set<String> resources = reflections.getResources(Pattern.compile(".*\\.js"));
-		for (String resource : resources) {
-			if (resources.contains("moment.js")) {
-				try (Reader reader = new InputStreamReader(
-						Thread.currentThread().getContextClassLoader().getResourceAsStream(resource))) {
-					Context.getCurrentContext().evaluateReader(variableScope.getScope(), reader, "moment.js", 1, null);
-				} catch (IOException e) {
-					LOGGER.error("Failed to load lib, so ignored.", e);
+	private synchronized String initLibrary() throws IOException {
+		if (momentJsString == null) {
+			Reflections reflections = new Reflections(new ConfigurationBuilder().setScanners(new ResourcesScanner())
+					.setUrls(ClasspathHelper.forPackage("com.samsung.sds.brightics.common.workflow.flowrunner.jslib")));
+			Set<String> resources = reflections.getResources(Pattern.compile(".*\\.js"));
+			for (String resource : resources) {
+				if (resources.contains("moment.js")) {
+					BufferedReader rd = new BufferedReader(new InputStreamReader(
+							Thread.currentThread().getContextClassLoader().getResourceAsStream(resource)));
+					String line;
+					StringJoiner script = new StringJoiner(LINE_SEPARATOR);
+					while ((line = rd.readLine()) != null) {
+						script.add(line);
+					}
+					rd.close();
+					momentJsString = script.toString();
 				}
 			}
 		}
-		
-		Context.exit();
+		return momentJsString;
+	}
+
+	@Override
+	public void loadLibrary(VariableScope variableScope) {
+		try {
+			Context.enter();
+			initLibrary();
+			Context.getCurrentContext().evaluateString(variableScope.getScope(), momentJsString, "moment.js", 1, null);
+			Context.exit();
+		} catch (Exception e) {
+			LOGGER.error("Failed to load lib, so ignored.", e);
+		}
 	}
 }
