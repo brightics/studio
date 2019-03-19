d8a86b5933277359a4576f42211dc8fdbf95415d
jinhwanhan <jinhwan.han@samsung.com>
2019-03-19 14:43:01 +0900

    add new options for resize

--- function/python/brightics/function/io/image.py
+++ function/python/brightics/function/io/image.py
@@ -30,30 +30,87 @@ def image_load(path, labeling='label'):
     return {'out_table': out_df}
 
 
-def resize(table, input_col, dsize=(100, 100)):
+# size
+# fixed-size (x,y)
+# fixed-ratio (x,y)
+# min-x
+# min-y
+# max-x
+# max-y
+#
+# shrink
+# scailing / cropping
+# cropping
+# (center
+#  left+top / center+top / right+top
+#  left+center / right+center
+#  left+bottom / center+bottom / right+bottom
+#
+#  zoom
+#  scailing / padding
+#  cropping
+#  (center
+#  left+top / center+top / right+top
+#  left+center / right+center
+#  left+bottom / center+bottom / right+bottom
+#
+#  target_size : fixed-size, fixed-ratio, min, x-min, y-min
+#  shrink_type : scailing, cropping-center, cropping-left-top, cropping-center-top, cropping-right-top
+#                cropping-left-center, cropping-right-center,
+#                cropping-left-bottom, cropping-center-bottom, cropping-right-bottom
+#  zoom_type : scailing, cropping-center, cropping-left-top, cropping-center-top, cropping-right-top
+#              cropping-left-center, cropping-right-center,
+#              cropping-left-bottom, cropping-center-bottom, cropping-right-bottom
+def resize(table, input_col, size_type='fixed-ratio', dsize=(1.0, 1.0), shrink_type='scailing', zoom_type='scailing'):
     if not is_image_col(table, input_col):
         raise ValueError("input column {} is not an image column.".format(input_col))
 
-    def _resize_img(img_byte, dsize):
-        img_np = byte_to_img(img_byte)
-        img_x = img_np.shape[0]
-        img_y = img_np.shape[1]
+    def _resize_img(img, size_type, dsize):
+        # img_np = byte_to_img(img_byte)
+        org_x = img.shape[0]
+        org_y = img.shape[1]
+        size_org = org_x * org_y
 
-        size_org = img_x * img_y
-        size_new = dsize[0] * dsize[1]
-        if size_org == size_new:
+        if size_type == 'size':
+            new_x = dsize[0]
+            new_y = dsize[1]
+        elif size_type == 'ratio':
+            new_x = int(org_x * dsize[0])
+            new_y = int(org_x * dsize[1])
+        else:
+            new_x = dsize[0]
+            new_y = dsize[1]
+
+        size_new = new_x * new_y
+        if org_x == new_x and org_y == new_y:
             # no change
-            img_resized = img_np
+            img_resized = img
         elif size_org > size_new:
             # make smaller
-            img_resized = cv2.resize(img_np, dsize=dsize, interpolation=cv2.INTER_AREA)
+            img_resized = cv2.resize(img, dsize=(new_x, new_y), interpolation=cv2.INTER_AREA)
         else:
             # make larger
-            img_resized = cv2.resize(img_np, dsize=dsize, interpolation=cv2.INTER_CUBIC)
+            img_resized = cv2.resize(img, dsize=(new_x, new_y), interpolation=cv2.INTER_LINEAR)
 
         return img_to_byte(img_resized)
 
-    resized_imgs = [_resize_img(x, dsize=dsize) for x in table[input_col]]
+    def _cropping_img(img_byte, crop_type, dsize):
+        pass
+
+    def _padding_img(img_byte, crop_type, dsize):
+        pass
+
+    npy_imgs = [byte_to_img(x) for x in table[input_col]]
+
+    if size_type == 'min':
+        dsize_x = min([x.shape[0] for x in npy_imgs])
+        dsize_y = min([x.shape[1] for x in npy_imgs])
+        resized_imgs = [_resize_img(x, size_type=size_type, dsize=(dsize_x, dsize_y)) for x in npy_imgs]
+    elif size_type == 'fixed-ratio':
+        resized_imgs = [_resize_img(x, size_type='ratio', dsize=dsize) for x in npy_imgs]
+    else:
+        resized_imgs = [_resize_img(x, size_type='size', dsize=dsize) for x in npy_imgs]
+
     table['image_resized'] = resized_imgs
 
     return {'out_table': table}
--- function/python/brightics/function/io/test/image_test.py
+++ function/python/brightics/function/io/test/image_test.py
@@ -4,11 +4,15 @@ import matplotlib.pyplot as plt
 from brightics.common.image import byte_to_img
 from brightics.function.io import image_load, resize
 import pandas as pd
+import pyarrow as pa
+import pyarrow.parquet as pq
+import random
+import time
 
 
 class ImageTest(unittest.TestCase):
-    # image_path = '''D:/dev/datasets/fruits-sampled'''
-    image_path = '''D:/dev/datasets/fruits-360/Training'''
+    image_path = '''D:/dev/datasets/fruits-sampled'''
+    # image_path = '''D:/dev/datasets/fruits-360/Training'''
     image_files = glob.glob('''{}/*/*'''.format(image_path))
 
     class BrighticsDataset(pd.DataFrame):
@@ -32,10 +36,39 @@ class ImageTest(unittest.TestCase):
 
     def test_resize(self):
         test_table = image_load(self.image_path)['out_table']
+        print('All images are loaded.')
         org_img = byte_to_img(test_table['image'][0])
-        resized = resize(test_table, 'image', dsize=(180, 180))['out_table']
+        # resized = resize(test_table, 'image', size_type='fixed-ratio', dsize=(0.8, 0.8))['out_table']
+        resized = resize(test_table, 'image', size_type='min')['out_table']
 
         resized_img = byte_to_img(resized['image_resized'][0])
         print(resized)
         print(org_img.shape)
         print(resized_img.shape)
+
+    def test_multiprocess(self):
+        out_table = image_load(self.image_path)['out_table']
+        out_table['part'] = [random.randint(0, 10) for i in range(0, len(out_table))]
+        out_arrow = pa.Table.from_pandas(out_table, nthreads=24)
+        # pq.write_table(out_arrow, 'out_data.pq')
+        ts0 = time.perf_counter()
+        pq.write_to_dataset(out_arrow, 'parquet/out_data')
+        t0 = time.perf_counter() - ts0
+
+        ts1 = time.perf_counter()
+        pq.write_table(out_arrow, 'parquet/out_table.pq')
+        t1 = time.perf_counter() - ts1
+
+        print('write_to_dataset : {}'.format(t0))
+        print('write_table : {}'.format(t1))
+
+        ts0 = time.perf_counter()
+        print(pd.read_parquet('parquet/out_data', use_threads=True)[:20])
+        t0 = time.perf_counter() - ts0
+
+        ts1 = time.perf_counter()
+        print(pd.read_parquet('parquet/out_table.pq', use_threads=True)[:20])
+        t1 = time.perf_counter() - ts1
+
+        print('read_dataset : {}'.format(t0))
+        print('read_table : {}'.format(t1))
