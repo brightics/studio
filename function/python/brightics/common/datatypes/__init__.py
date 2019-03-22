0323cf4f0722eb8ae018de3a66d37b7a755f996f
Sungjun Kim <sungjun5.kim@samsung.net>
2019-03-22 17:36:04 +0900

    image type, load_from_directory test

---
+++ function/python/brightics/common/datatypes/__init__.py
@@ -0,0 +1,2 @@
+BRTC_CODE = 'brightics'.encode('utf-8')
+BRTC_CODE_SIZE = len(BRTC_CODE)
---
+++ function/python/brightics/common/datatypes/image.py
@@ -0,0 +1,62 @@
+import io
+import struct
+
+import cv2
+import numpy as np
+from brightics.common.datatypes import BRTC_CODE, BRTC_CODE_SIZE
+
+
+class Image(object):
+    _data_type = 0
+    _header_format = '<{}sBIII'
+    _pack_format = ''.join([_header_format,'I{}s', 'I{}s', 'I{}s'])
+
+    def __init__(self, arr, origin=None, mode=None):
+        self.data = arr
+        self.height, self.width, self.n_channels = self.data.shape
+        self.origin = None
+        self.mode = 'BRG'
+        if origin is not None:
+            self.origin = origin
+        if mode is not None:
+            self.mode = mode
+
+    def tobytes(self):
+        '''
+        brtc_code(16)::data_type(1)::height(4)::width(4)::n_channels(4)::
+        mode_size(4)::mode(mode_size)::
+        origin_size(4)::origin(origin_size)::
+        data_size(4)::data(data_size)
+        '''
+        data_bytes = self.data.tobytes()
+        data_size = len(data_bytes)
+        origin_bytes = self.origin.encode('utf-8')
+        origin_size = 1 if origin_bytes is None else len(origin_bytes)
+
+        mode_bytes = self.mode.encode('utf-8')
+        mode_bytes_size = len(mode_bytes)
+        pack_format = self._pack_format.format(BRTC_CODE_SIZE, mode_bytes_size, origin_size, data_size)
+
+        return struct.pack(pack_format,
+            BRTC_CODE, self._data_type, self.height, self.width, self.n_channels,
+            mode_bytes_size, mode_bytes,
+            origin_size, origin_bytes,
+            data_size, data_bytes)
+
+    @classmethod
+    def from_bytes(cls, b):
+        buf = io.BytesIO(b)
+        brtc_code, data_type, height, width, n_channels = struct.unpack(cls._header_format.format(BRTC_CODE_SIZE), buf.read(BRTC_CODE_SIZE + 1 + 4 + 4 + 4))
+        
+#         if brtc_code != BRTC_CODE:
+#             raise Exception("Unknown data")
+#         if data_type != cls._data_type:
+#             raise Exception('This is not an Image')
+        mode_size = struct.unpack('<I', buf.read(4))[0]
+        mode = struct.unpack('<{}s'.format(mode_size), buf.read(mode_size))[0].decode('utf-8')
+        origin_size = struct.unpack('<I', buf.read(4))[0]
+        origin = struct.unpack('<{}s'.format(origin_size), buf.read(origin_size))[0].decode('utf-8')
+        data_size = struct.unpack('<I', buf.read(4))[0]
+        data = np.frombuffer(struct.unpack('<{}s'.format(data_size), buf.read(data_size))[0],np.uint8).reshape(height, width, n_channels)
+
+        return Image(data, origin, mode)
