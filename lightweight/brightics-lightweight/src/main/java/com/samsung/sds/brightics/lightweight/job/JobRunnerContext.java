8d2c4f1dcaf12ae52fc66c8f23cb999ed8acafb8
hoonki <hoon.im@samsung.com>
2019-05-14 18:03:48 +0900

    Prepend license comment to all java

--- lightweight/brightics-lightweight/src/main/java/com/samsung/sds/brightics/lightweight/job/JobRunnerContext.java
+++ lightweight/brightics-lightweight/src/main/java/com/samsung/sds/brightics/lightweight/job/JobRunnerContext.java
@@ -1,3 +1,19 @@
+/*
+    Copyright 2019 Samsung SDS
+    
+    Licensed under the Apache License, Version 2.0 (the "License");
+    you may not use this file except in compliance with the License.
+    You may obtain a copy of the License at
+    
+        http://www.apache.org/licenses/LICENSE-2.0
+    
+    Unless required by applicable law or agreed to in writing, software
+    distributed under the License is distributed on an "AS IS" BASIS,
+    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+    See the License for the specific language governing permissions and
+    limitations under the License.
+*/
+
 package com.samsung.sds.brightics.lightweight.job;
 
 import java.io.File;
@@ -18,8 +34,8 @@ import com.samsung.sds.brightics.common.core.util.JsonUtil;
 import com.samsung.sds.brightics.common.core.util.SystemEnvUtil;
 import com.samsung.sds.brightics.common.workflow.flowrunner.JobRunnerBuilder;
 import com.samsung.sds.brightics.common.workflow.flowrunner.JobRunnerConfig;
-import com.samsung.sds.brightics.common.workflow.flowrunner.data.PreparedData;
 import com.samsung.sds.brightics.common.workflow.flowrunner.vo.JobParam;
+import com.samsung.sds.brightics.common.workflow.flowrunner.vo.PreparedDataVO;
 
 /**
  * The class which creates job runner for lightweight analysis. This class
@@ -91,10 +107,10 @@ public class JobRunnerContext {
 			String eventloadData) throws Exception {
 		JobParam jobParam = convertStringToJobParam(modelJsonString);
 		if (eventloadData != null && !eventloadData.isEmpty()) {
-			List<PreparedData> preparedDatas = new ArrayList<>();
+			List<PreparedDataVO> preparedDatas = new ArrayList<>();
 			JsonArray dataArray = JsonUtil.fromJson(eventloadData, JsonArray.class);
 			for (JsonElement dataElm : dataArray) {
-				preparedDatas.add(JsonUtil.fromJson(dataElm, PreparedData.class));
+				preparedDatas.add(JsonUtil.fromJson(dataElm, PreparedDataVO.class));
 			}
 			jobParam.setDatas(preparedDatas);
 		}
