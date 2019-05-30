6fe77f7fc35d02dbd7ab2f00ac349819098eeca8
jooyeon chung <jy1106.chung@samsung.com>
2019-05-30 17:25:57 +0900

    textanalytics init.py changed

--- function/python/brightics/function/textanalytics/__init__.py
+++ function/python/brightics/function/textanalytics/__init__.py
@@ -1,14 +1,22 @@
+"""
+    Copyright 2019 Samsung SDS
+    
+    Licensed under the Apache License, Version 2.0 (the "License");
+    you may not use this file except in compliance with the License.
+    You may obtain a copy of the License at
+    
+        http://www.apache.org/licenses/LICENSE-2.0
+    
+    Unless required by applicable law or agreed to in writing, software
+    distributed under the License is distributed on an "AS IS" BASIS,
+    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+    See the License for the specific language governing permissions and
+    limitations under the License.
+"""
+
+
+
 from .ngram import ngram
 from .lda import lda
 from .tfidf import tfidf
 from .tfidf import tfidf2
-from .preprocess import preprocess
-from .word2vec import word_to_vec
-from .word2vec import word_to_vec_model
-from .tw_tokenizer import tw_tokenizer
-from .stop_words_remove import stop_words_remove
-from .synonym_remove import synonym_remove
-from .bow import bow
-from .doc_doc_mtx import doc_doc_mtx
-from .doc_term_mtx import doc_term_mtx
-from .term_term_mtx import term_term_mtx
