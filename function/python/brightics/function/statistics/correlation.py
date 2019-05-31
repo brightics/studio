e3c2a6e5995a01ba6b04a09ec6168518df5cd33f
Seungro Joo <beartan@postech.ac.kr>
2019-05-31 14:20:08 +0900

    Add display_plt parameter in Correlation

--- function/python/brightics/function/statistics/correlation.py
+++ function/python/brightics/function/statistics/correlation.py
@@ -38,12 +38,9 @@ def correlation(table, group_by=None, **params):
         return _correlation(table, **params)
 
 
-def _correlation(table, vars, method='pearson', height=2.5, corr_prec=2):
+def _correlation(table, vars, method='pearson', display_plt = True, height=2.5, corr_prec=2):
+
     size = len(vars)
-    
-    s_default = plt.rcParams['lines.markersize'] ** 2.
-    scatter_kws = {"s": s_default * height / 6.4}
-    
     result_arr = []
     
     for i in range(size): 
@@ -52,58 +49,71 @@ def _correlation(table, vars, method='pearson', height=2.5, corr_prec=2):
                 r, p = stats.pearsonr(table[vars[i]], table[vars[j]])
             elif method == 'spearman':
                 r, p = stats.spearmanr(table[vars[i]], table[vars[j]])
-            elif method == 'kendall':
+            else:
                 r, p = stats.kendalltau(table[vars[i]], table[vars[j]])
             
             result_arr.append([vars[i], vars[j], r, p])    
             
     df_result = pd.DataFrame(result_arr, columns=['x', 'y', 'corr', 'p_value'])
-    
-    def corr(x, y, **kwargs):
-        if kwargs['method'] == 'pearson':
-            r, p = stats.pearsonr(x, y)
-        elif kwargs['method'] == 'spearman':
-            r, p = stats.spearmanr(x, y)
-        elif kwargs['method'] == 'kendall':
-            r, p = stats.kendalltau(x, y)
+
+    rb = BrtcReprBuilder()    
+    if display_plt:
+        s_default = plt.rcParams['lines.markersize'] ** 2.
+        scatter_kws = {"s": s_default * height / 6.4}
         
-        p_stars = ''
-        if p <= 0.05:
-            p_stars = '*'
-        if p <= 0.01:
-            p_stars = '**'
-        if p <= 0.001:
-            p_stars = '***'
+        def corr(x, y, **kwargs):
+            if kwargs['method'] == 'pearson':
+                r, p = stats.pearsonr(x, y)
+            elif kwargs['method'] == 'spearman':
+                r, p = stats.spearmanr(x, y)
+            else:
+                r, p = stats.kendalltau(x, y)
             
-        corr_text = '{:.{prec}f}'.format(r, prec=corr_prec)
-        font_size = abs(r) * 15 * 2 / corr_prec + 5
-        ax = plt.gca()
-        ax.annotate(corr_text, [.5, .5, ], xycoords="axes fraction",
-                    ha='center', va='center', fontsize=font_size * height)
-        ax.annotate(p_stars, xy=(0.65, 0.6), xycoords=ax.transAxes, color='red', fontsize=17 * height)
-     
-    g = sns.PairGrid(table, vars=vars, height=height)
-    g.map_diag(sns.distplot)
-    if method == 'pearson':
-        g.map_lower(sns.regplot, scatter_kws=scatter_kws)
+            p_stars = ''
+            if p <= 0.05:
+                p_stars = '*'
+            if p <= 0.01:
+                p_stars = '**'
+            if p <= 0.001:
+                p_stars = '***'
+                
+            corr_text = '{:.{prec}f}'.format(r, prec=corr_prec)
+            font_size = abs(r) * 15 * 2 / corr_prec + 5
+            ax = plt.gca()
+            ax.annotate(corr_text, [.5, .5, ], xycoords="axes fraction",
+                        ha='center', va='center', fontsize=font_size * height)
+            ax.annotate(p_stars, xy=(0.65, 0.6), xycoords=ax.transAxes, color='red', fontsize=17 * height)
+         
+        g = sns.PairGrid(table, vars=vars, height=height)
+        g.map_diag(sns.distplot)
+        if method == 'pearson':
+            g.map_lower(sns.regplot, scatter_kws=scatter_kws)
+        else:
+            g.map_lower(sns.regplot, lowess=True, scatter_kws=scatter_kws) 
+        g.map_upper(corr, method=method)
+        
+        fig_corr = plt2MD(plt)
+        plt.clf()
+        
+        rb.addMD(strip_margin(
+            """ ## Correlation Results
+            | ### Correlation Matrix
+            | {fig_corr}
+            |
+            | ### Correlation Table
+            | {table}
+            """.format(fig_corr=fig_corr, table=pandasDF2MD(df_result))))
+        
+        params = {'vars':vars, 'method':method, 'height':height}
+        
     else:
-        g.map_lower(sns.regplot, lowess=True, scatter_kws=scatter_kws) 
-    g.map_upper(corr, method=method)
-    
-    fig_corr = plt2MD(plt)
-    plt.clf()
-    
-    rb = BrtcReprBuilder()
-    rb.addMD(strip_margin(
-        """ ## Correlation Results
-        | ### Correlation Matrix
-        | {fig_corr}
-        |
-        | ### Correlation Table
-        | {table}
-        """.format(fig_corr=fig_corr, table=pandasDF2MD(df_result))))
-    
-    params = {'vars':vars, 'method':method, 'height':height}
+        rb.addMD(strip_margin(
+            """ ## Correlation Results
+            | ### Correlation Table
+            | {table}
+            """.format(table=pandasDF2MD(df_result))))
+        
+        params = {'vars':vars, 'method':method}       
     
     res = dict()
     res['params'] = params
