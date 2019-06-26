66fb36a325a230579fb8900192bb4d16fdd45dc9
hyoxt121 <36657450+hyoxt121@users.noreply.github.com>
2019-06-26 14:44:01 +0900

    Add files via upload

--- function/python/brightics/function/classification/random_forest_classification.py
+++ function/python/brightics/function/classification/random_forest_classification.py
@@ -17,8 +17,8 @@
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
-import sklearn.utils
-from sklearn.ensemble import RandomForestClassifier 
+from sklearn.utils.multiclass import type_of_target
+from sklearn.ensemble import RandomForestClassifier
 from brightics.common.repr import BrtcReprBuilder
 from brightics.common.repr import strip_margin
 from brightics.common.repr import plt2MD
@@ -29,6 +29,7 @@ from brightics.common.utils import get_default_from_parameters_if_required
 from brightics.common.validation import validate
 from brightics.common.validation import greater_than_or_equal_to
 from brightics.common.validation import raise_error
+from brightics.common.classify_input_type import check_col_type
 
 
 def random_forest_classification_train(table, group_by=None, **params):
@@ -47,18 +48,18 @@ def random_forest_classification_train(table, group_by=None, **params):
         return _random_forest_classification_train(table, **params)
 
 
-def _plot_feature_importances(feature_cols, classifier):
+def _plot_feature_importances(feature_names, classifier):
     
     feature_importance = classifier.feature_importances_
     indices = np.argsort(feature_importance)
-    sorted_feature_cols = np.array(feature_cols)[indices]
+    sorted_feature_names = np.array(feature_names)[indices]
     
     n_features = classifier.n_features_
     plt.barh(range(n_features), feature_importance[indices], color='b', align='center')
     for i, v in enumerate(feature_importance[indices]):
         plt.text(v, i, " {:.2f}".format(v), color='b', va='center', fontweight='bold')
     
-    plt.yticks(np.arange(n_features), sorted_feature_cols)
+    plt.yticks(np.arange(n_features), sorted_feature_names)
     plt.xlabel("Feature importance")
     plt.ylabel("Feature")
     plt.tight_layout()
@@ -70,19 +71,20 @@ def _plot_feature_importances(feature_cols, classifier):
 def _random_forest_classification_train(table, feature_cols, label_col,
                                  n_estimators=10, criterion="gini", max_depth=None, min_samples_split=2, min_samples_leaf=1,
                                  min_weight_fraction_leaf=0, max_features="sqrt",
-                                 max_leaf_nodes=None, min_impurity_decrease=0, class_weight=None, random_state=None):   
+                                 max_leaf_nodes=None, min_impurity_decrease=0, class_weight=None, random_state=None):
     
-    X_train = table[feature_cols]
-    y_train = table[label_col]   
+    feature_names, features_train = check_col_type(table, feature_cols)
+    #X_train = table[feature_cols]
+    y_train = table[label_col]
 
-    if(sklearn.utils.multiclass.type_of_target(y_train) == 'continuous'):
+    if(type_of_target(y_train) == 'continuous'):
         raise_error('0718', 'label_col')
     
     if max_features == "n":
         max_features = None
         
-    class_labels = list(set(y_train)) 
-    if class_weight is not None: 
+    class_labels = list(set(y_train))
+    if class_weight is not None:
         if len(class_weight) != len(class_labels):
             raise ValueError("Number of class weights should match number of labels.")
         else:
@@ -95,8 +97,8 @@ def _random_forest_classification_train(table, feature_cols, label_col,
                 class_weight = {classes[0]: weights[0]}
                 
                 for i in range(1, len(classes)):
-                    class_weight[classes[i]] = weights[i]  
-    
+                    class_weight[classes[i]] = weights[i]
+            
     classifier = RandomForestClassifier(n_estimators=n_estimators,
                                         criterion=criterion,
                                         max_depth=max_depth,
@@ -108,7 +110,7 @@ def _random_forest_classification_train(table, feature_cols, label_col,
                                         min_impurity_decrease=min_impurity_decrease,
                                         class_weight=class_weight,
                                         random_state=random_state)
-    classifier.fit(X_train, y_train) 
+    classifier.fit(features_train, y_train)
 
     params = {'feature_cols': feature_cols,
              'label_col': label_col,
@@ -128,7 +130,7 @@ def _random_forest_classification_train(table, feature_cols, label_col,
     model['classifier'] = classifier
     model['params'] = params
 
-    fig_feature_importances = _plot_feature_importances(feature_cols, classifier)
+    fig_feature_importances = _plot_feature_importances(feature_names, classifier)
            
     rb = BrtcReprBuilder()
     rb.addMD(strip_margin("""
@@ -142,7 +144,7 @@ def _random_forest_classification_train(table, feature_cols, label_col,
     |
     """.format(params=dict2MD(params), fig_feature_importances=fig_feature_importances)))
         
-    model['_repr_brtc_'] = rb.get()   
+    model['_repr_brtc_'] = rb.get()
                
     return {'model' : model}
 
@@ -158,9 +160,11 @@ def random_forest_classification_predict(table, model, **params):
 def _random_forest_classification_predict(table, model, pred_col_name='prediction', prob_col_prefix='probability', suffix='index'):
     out_table = table.copy()
     classifier = model['classifier']
-    test_data = table[model['params']['feature_cols']]
     
-    out_table[pred_col_name] = classifier.predict(test_data)   
+    feature_cols = model['params']['feature_cols']
+    feature_names, features_test = check_col_type(table, feature_cols)
+    
+    out_table[pred_col_name] = classifier.predict(features_test)
     
     classes = classifier.classes_
     if suffix == 'index':
@@ -168,8 +172,8 @@ def _random_forest_classification_predict(table, model, pred_col_name='predictio
     else:
         suffixes = classes
         
-    prob = classifier.predict_proba(test_data)
-    prob_col_name = ['{prob_col_prefix}_{suffix}'.format(prob_col_prefix=prob_col_prefix, suffix=suffix) for suffix in suffixes]    
+    prob = classifier.predict_proba(features_test)
+    prob_col_name = ['{prob_col_prefix}_{suffix}'.format(prob_col_prefix=prob_col_prefix, suffix=suffix) for suffix in suffixes]
     out_col_prob = pd.DataFrame(data=prob, columns=prob_col_name)
 
     out_table = pd.concat([out_table, out_col_prob], axis=1)
