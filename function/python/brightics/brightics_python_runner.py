71239bd4a67dc6cd5de6bd510772d4f5c139ee1f
power4454 <45587294+power4454@users.noreply.github.com>
2019-12-20 15:52:56 +0900

    Update brightics_python_runner.py

--- function/python/brightics/brightics_python_runner.py
+++ function/python/brightics/brightics_python_runner.py
@@ -205,8 +205,7 @@ if __name__ == '__main__':
 
                 brtc_java_gateway.notify_python_process_finished(result, is_exception[0], is_exception[1])
             except BrighticsFunctionException as bfe:
-                brtc_java_gateway.notify_brightics_function_exception(traceback.format_exc(),json.dumps(bfe.errors) )
-                #brtc_java_gateway.notify_python_process_finished(traceback.format_exc(), True, json.dumps(bfe.errors))
+                brtc_java_gateway.notify_brightics_function_exception(traceback.format_exc(), json.dumps(bfe.errors) )
                 brtc_java_gateway.logger.info("[Python] " + traceback.format_exc())
             except BrighticsCoreException as bce:
                 brtc_java_gateway.notify_brightics_core_exception(traceback.format_exc(), str(bce.code), bce.message)
