87cea7a2d89e55f7932b74a066e346a881f08fb5
Kihyo Moon <36657450+hyoxt121@users.noreply.github.com>
2019-12-27 14:46:51 +0900

    Update __init__.py

--- function/python/brightics/function/transform/__init__.py
+++ function/python/brightics/function/transform/__init__.py
@@ -31,4 +31,3 @@ from .sql import execute as sql_execute
 from .sql import execute2 as sql_execute2
 from .svd import svd
 from .transpose_time_series import transpose_time_series
-from .over_sampling import over_sampling
