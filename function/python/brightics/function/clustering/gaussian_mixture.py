e581fc8e8fb72e61e01dacad5585caf2359ed5d6
Kihyo Moon <36657450+hyoxt121@users.noreply.github.com>
2020-03-14 21:58:39 +0900

    fix a bug

--- function/python/brightics/function/clustering/gaussian_mixture.py
+++ function/python/brightics/function/clustering/gaussian_mixture.py
@@ -68,7 +68,7 @@ def _gaussian_mixture_train(table, input_cols, number_of_components=1, covarianc
     out_table['component_number'] = comp_num_arr
     out_table['weight'] = gmm.weights_
     out_table['mean_coordinate'] = mean_arr
-    # temporary string type to covar_arry because it is array(array(array(double))) which is not supported so it causes problems
+    # temporary string type to covar_arr because it is array(array(array(double))) which is not supported so it causes problems
     out_table['covariance_matrix'] = str(covar_arr)
     
     rb = BrtcReprBuilder()
