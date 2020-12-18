b0d0a80f722f06dac4c69816853d68d297a4d132
jmk09 <jmk09.jung@samsung.com>
2020-12-18 13:34:39 +0900

    work cloud 차트 추가

---
+++ va-server/visual-analytics/public/js/va/brightics-va-main.js
@@ -0,0 +1,214 @@
+/*   Brightics VA Build 201218 */
+/*   Brightics VA Build 201208 */
+/*   Brightics VA Build 201208 */
+/*   Brightics VA Build 201208 */
+/*   Brightics VA Build 201208 */
+/*   Brightics VA Build 201119 */
+/*   Brightics VA Build 201119 */
+/*   Brightics VA Build 201119 */
+/*   Brightics VA Build 201119 */
+/*   Brightics VA Build 201119 */
+/*   Brightics VA Build 201118 */
+/*   Brightics VA Build 201118 */
+/*   Brightics VA Build 201118 */
+/*   Brightics VA Build 201118 */
+/*   Brightics VA Build 201118 */
+/*   Brightics VA Build 201118 */
+/*   Brightics VA Build 201118 */
+/*   Brightics VA Build 201118 */
+/*   Brightics VA Build 201118 */
+/*   Brightics VA Build 201118 */
+/*   Brightics VA Build 201118 */
+/*   Brightics VA Build 201118 */
+/*   Brightics VA Build 201118 */
+/*   Brightics VA Build 201118 */
+/*   Brightics VA Build 201118 */
+/*   Brightics VA Build 201118 */
+/*   Brightics VA Build 201118 */
+/*   Brightics VA Build 201118 */
+/*   Brightics VA Build 201118 */
+/*   Brightics VA Build 201118 */
+/*   Brightics VA Build 201118 */
+/*   Brightics VA Build 201118 */
+/*   Brightics VA Build 201118 */
+/*   Brightics VA Build 201118 */
+/*   Brightics VA Build 201118 */
+/*   Brightics VA Build 201118 */
+/*   Brightics VA Build 201118 */
+/*   Brightics VA Build 201118 */
+/*   Brightics VA Build 201118 */
+/*   Brightics VA Build 201118 */
+/*   Brightics VA Build 201118 */
+/*   Brightics VA Build 201118 */
+/*   Brightics VA Build 201118 */
+/*   Brightics VA Build 201118 */
+/*   Brightics VA Build 201118 */
+/*   Brightics VA Build 201118 */
+/*   Brightics VA Build 201118 */
+/*   Brightics VA Build 200821 */
+/*   Brightics VA Build 200821 */
+/*   Brightics VA Build 200821 */
+/*   Brightics VA Build 200821 */
+/*   Brightics VA Build 200821 */
+/*   Brightics VA Build 200821 */
+/*   Brightics VA Build 200821 */
+/*   Brightics VA Build 200821 */
+/*   Brightics VA Build 200821 */
+/*   Brightics VA Build 200821 */
+/*   Brightics VA Build 200821 */
+/*   Brightics VA Build 200821 */
+/*   Brightics VA Build 200821 */
+/*   Brightics VA Build 200821 */
+/*   Brightics VA Build 200821 */
+/*   Brightics VA Build 200821 */
+/*   Brightics VA Build 200821 */
+/*   Brightics VA Build 200821 */
+/*   Brightics VA Build 200424 */
+/*   Brightics VA Build 200424 */
+/*   Brightics VA Build 200424 */
+/*   Brightics VA Build 200424 */
+/*   Brightics VA Build 200424 */
+/*   Brightics VA Build 200424 */
+/*   Brightics VA Build 200420 */
+/*   Brightics VA Build 200414 */
+/*   Brightics VA Build 200413 */
+/*   Brightics VA Build 200406 */
+/*   Brightics VA Build 200406 */
+/*   Brightics VA Build 200406 */
+/*   Brightics VA Build 200406 */
+/*   Brightics VA Build 200406 */
+/*   Brightics VA Build 200406 */
+/*   Brightics VA Build 200406 */
+/*   Brightics VA Build 200406 */
+/*   Brightics VA Build 200406 */
+/*   Brightics VA Build 200406 */
+/*   Brightics VA Build 200403 */
+/*   Brightics VA Build 200403 */
+/*   Brightics VA Build 200403 */
+/*   Brightics VA Build 200403 */
+/*   Brightics VA Build 200403 */
+/*   Brightics VA Build 200403 */
+/*   Brightics VA Build 200403 */
+/*   Brightics VA Build 200403 */
+/*   Brightics VA Build 200403 */
+/*   Brightics VA Build 200403 */
+/*   Brightics VA Build 200403 */
+/*   Brightics VA Build 200403 */
+/*   Brightics VA Build 200403 */
+/*   Brightics VA Build 200403 */
+/*   Brightics VA Build 200403 */
+/*   Brightics VA Build 200403 */
+/*   Brightics VA Build 200403 */
+/*   Brightics VA Build 200403 */
+/*   Brightics VA Build 200403 */
+/*   Brightics VA Build 200403 */
+/*   Brightics VA Build 200403 */
+/*   Brightics VA Build 200403 */
+/*   Brightics VA Build 200403 */
+/*   Brightics VA Build 200403 */
+/*   Brightics VA Build 200403 */
+/*   Brightics VA Build 200402 */
+/*   Brightics VA Build 200402 */
+/*   Brightics VA Build 200402 */
+/*   Brightics VA Build 200402 */
+/*   Brightics VA Build 200331 */
+/*   Brightics VA Build 200331 */
+/*   Brightics VA Build 200331 */
+/*   Brightics VA Build 200331 */
+/*   Brightics VA Build 200327 */
+/*   Brightics VA Build 200327 */
+/*   Brightics VA Build 200327 */
+/*   Brightics VA Build 200327 */
+/*   Brightics VA Build 200327 */
+/*   Brightics VA Build 200325 */
+/*   Brightics VA Build 200323 */
+/*   Brightics VA Build 200317 */
+/*   Brightics VA Build 200312 */
+/*   Brightics VA Build 200312 */
+/*   Brightics VA Build 200311 */
+/*   Brightics VA Build 200311 */
+/*   Brightics VA Build 200306 */
+/*   Brightics VA Build 200306 */
+/*   Brightics VA Build 200306 */
+/*   Brightics VA Build 200306 */
+/*   Brightics VA Build 200306 */
+/*   Brightics VA Build 200306 */
+/*   Brightics VA Build 200306 */
+/*   Brightics VA Build 200306 */
+/*   Brightics VA Build 200306 */
+/*   Brightics VA Build 191104 */
+/*   Brightics VA Build 191104 */
+/*   Brightics VA Build 191104 */
+/*   Brightics VA Build 191104 */
+/*   Brightics VA Build 191104 */
+/*   Brightics VA Build 191104 */
+/*   Brightics VA Build 191104 */
+/*   Brightics VA Build 191104 */
+/*   Brightics VA Build 191104 */
+/*   Brightics VA Build 191104 */
+/*   Brightics VA Build 191104 */
+/*   Brightics VA Build 191104 */
+/*   Brightics VA Build 191104 */
+/*   Brightics VA Build 191104 */
+/*   Brightics VA Build 191104 */
+/*   Brightics VA Build 191104 */
+var Studio = (function ($parent, options) {
+    var instance = createInstance($parent, options);
+
+    function createInstance($parent, options) {
+        var object = new Brightics.VA.Studio($parent, options);
+        return object;
+    }
+
+    return {
+        getInstance: function () {
+            if (!instance) {
+                instance = createInstance($parent, options);
+            }
+            return instance;
+        },
+        getResourceManager: function () {
+            return this.getInstance().getResourceManager();
+        },
+        getSession: function () {
+            return this.getInstance().getSession();
+        },
+        getLayoutManager: function () {
+            return this.getInstance().getLayoutManager();
+        },
+        getPreference: function () {
+            return this.getInstance().getPreference();
+        },
+        getValidator: function () {
+            return this.getInstance().getValidator();
+        },
+        getResourceService: function () {
+            return this.getInstance().getResourceService();
+        },
+        getJobExecutor: function () {
+            return this.getInstance().getJobExecutor();
+        },
+        getEditorContainer: function () {
+            return this.getInstance().getEditorContainer();
+        },
+        getClipboardManager: function () {
+            return this.getInstance().getClipboardManager();
+        },
+        getActiveEditor: function () {
+            return this.getInstance().getActiveEditor();
+        }
+    };
+})($(".brtc-va-main"), {userId: user, permissions: JSON.parse(permissions), logLevel: logLevel, isPublish: window.isPublish});
+
+$(function () {
+    $('#brightics').perfectScrollbar();
+    $(window).on('resize', function () {
+        $('#brightics').perfectScrollbar('update');
+    });
+    var heartbeat = setInterval(function () {
+        if (!$.cookie('brightics.va.sid')) {
+            clearInterval(heartbeat);
+            $.get('/api/va/v2/users/my');
+        }
+    }, 60000);
+});
