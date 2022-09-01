6125e16fd36e7ca69a1f8ca4e59cabbfd8550434
장희준 <heejun.jang@samsung.com>
2022-09-01 12:39:10 +0900

    Update AgentUserService.java

--- api-server/brightics-server/src/main/java/com/samsung/sds/brightics/server/service/AgentUserService.java
+++ api-server/brightics-server/src/main/java/com/samsung/sds/brightics/server/service/AgentUserService.java
@@ -25,6 +25,7 @@ import org.apache.commons.lang3.StringUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Lazy;
 import org.springframework.stereotype.Service;
 
 import com.samsung.sds.brightics.common.core.exception.BrighticsCoreException;
@@ -42,6 +43,7 @@ public class AgentUserService {
 
 	private static final Logger logger = LoggerFactory.getLogger(AgentUserService.class);
 
+	@Lazy
 	@Autowired
 	private AgentService agentService;
 
